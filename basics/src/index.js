71725847143097d76f7b7f2e57adc9fb184b0ec3
omar jakmira <poweromar19942@gmail.com>
2019-12-08 15:05:50 +0200

    mutations

--- basics/src/index.js
+++ basics/src/index.js
@@ -1,111 +1,239 @@
-import { GraphQLServer } from "graphql-yoga"
+import { GraphQLServer } from 'graphql-yoga'
+import uuid from 'uuid/v4'
 
-//type || application schema
+// Scalar types - String, Boolean, Int, Float, ID
+
+// Demo user data
+const users = [{
+    id: '1',
+    name: 'Andrew',
+    email: 'andrew@example.com',
+    age: 27
+}, {
+    id: '2',
+    name: 'Sarah',
+    email: 'sarah@example.com'
+}, {
+    id: '3',
+    name: 'Mike',
+    email: 'mike@example.com'
+}]
+
+const posts = [{
+    id: '10',
+    title: 'GraphQL 101',
+    body: 'This is how to use GraphQL...',
+    published: true,
+    author: '1'
+}, {
+    id: '11',
+    title: 'GraphQL 201',
+    body: 'This is an advanced GraphQL post...',
+    published: false,
+    author: '1'
+}, {
+    id: '12',
+    title: 'Programming Music',
+    body: '',
+    published: false,
+    author: '2'
+}]
+
+const comments = [{
+    id: '102',
+    text: 'This worked well for me. Thanks!',
+    author: '3',
+    post: '10'
+}, {
+    id: '103',
+    text: 'Glad you enjoyed it.',
+    author: '1',
+    post: '10'
+}, {
+    id: '104',
+    text: 'This did no work.',
+    author: '2',
+    post: '11'
+}, {
+    id: '105',
+    text: 'Nevermind. I got it to work.',
+    author: '1',
+    post: '11'
+}]
+
+// Type definitions (schema)
 const typeDefs = `
     type Query {
-        greeting(name:String):String!
-        grades:[Int]!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        comments: [Comment!]!
         me: User!
-        posts(query:String):[Post!]!
-        users:[User]!
+        post: Post!
+    }
+
+    type Mutation{
+        createUser(name: String!,email: String!):User!
+        createPost(title:String!,body:String!,published:Boolean!,author:ID!):Post!
+        createComment(text:String!,author:ID!,post:ID!):Comment!
     }
 
     type User {
-        id:ID!
-        name:String!
-        email:String!
-        age:Int,
-        posts:[Post]
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+        posts: [Post!]
+        comments: [Comment!]
     }
 
     type Post {
-        id:ID!
-        title:String!
-        body:String!
-        author:User!
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
+        comments: [Comment!]!
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
     }
 `
-    //resolvers
 
-let posts = [{
-        id: 1,
-        title: "post",
-        body: "some text",
-        author: '1'
-    },
-    {
-        id: 3,
-        title: "post",
-        body: "some text",
-        author: '2'
-    }
-]
-
-let users = [{
-        id: '1',
-        name: "omar",
-        email: "o@o.com"
-    },
-    {
-        id: '2',
-        name: "mhd",
-        email: "m@m.com"
-    }
-]
+// Resolvers
 const resolvers = {
     Query: {
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users
+            }
+
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts
+            }
+
+            return posts.filter((post) => {
+                const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+                const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+                return isTitleMatch || isBodyMatch
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments
+        },
         me() {
             return {
-                id: 1,
-                name: "omar",
-                email: "o@o.com",
-                age: 20
+                id: '123098',
+                name: 'Mike',
+                email: 'mike@example.com'
             }
         },
-        users() {
-            return users
-        },
-        posts(_, args) {
-
-            return args.query == null ? posts :
-                posts.filter(post => post.title.toLowerCase().includes(args.query))
-        },
-        // graphql pass 4 arguments parent,args,context,info
-        greeting(_, args) {
-            return `welcome ${args.name}`
-        },
-
-        grades(parent, args, context, info) {
-            return [10, 12, 20, 30]
+        post() {
+            return {
+                id: '092',
+                title: 'GraphQL 101',
+                body: '',
+                published: false
+            }
         }
     },
-    //resolve relations for Post type
-    //this will call for each post when call relation properity
+    Mutation: {
+        createUser(parent, args, ctx, info) {
+            var emailTaken = users.some(u => u.email == args.email)
+            if (emailTaken)
+                throw new Error("email taken")
+            var user = {
+                id: uuid(),
+                name: args.name,
+                email: args.email
+            }
+            users.push(user)
+            return user
+        },
+        createPost(parent, args, ctx, info) {
+            var userExist = users.some(u => u.id == args.author)
+            if (!userExist)
+                throw new Error("user not exist")
+
+            const post = {
+                id: uuid(),
+                title: args.title,
+                body: args.body,
+                published: args.published,
+                author: args.author,
+            }
+            posts.push(post)
+            return post
+        },
+        createComment(parent, args, ctx, info) {
+            var userExist = users.some(u => u.id == args.author)
+            var postExist = posts.some(post =>
+                post.id === args.post && post.published
+            )
+            if (!userExist || !postExist)
+                throw new Error("user not exist or post not found")
+
+            const comment = {
+                id: uuid(),
+                text: args.text,
+                author: args.author,
+                post: args.post
+            }
+            comments.push(comment)
+            return comment
+        }
+    },
     Post: {
-        author(parent, args, context, info) {
-            console.log("post running")
-            return users.find(user => user.id === parent.author)
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.post === parent.id
+            })
+        }
+    },
+    Comment: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        },
+        post(parent, args, ctx, info) {
+            return posts.find((post) => {
+                return post.id === parent.post
+            })
         }
     },
     User: {
-        posts(parent, args, context, info) {
-            console.log("User running")
-            return posts.filter(post => post.author === parent.id)
+        posts(parent, args, ctx, info) {
+            return posts.filter((post) => {
+                return post.author === parent.id
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.author === parent.id
+            })
         }
     }
 }
 
-class Post {
-    constructor(id, title, body, author) {
-        this.id = id
-        this.title = title
-        this.body = body
-        this.author = author
-    }
-}
 const server = new GraphQLServer({
     typeDefs,
     resolvers
 })
 
-server.start(() => console.log("server running"))
+server.start(() => {
+    console.log('The server is up!')
+})
