81e05ff59b828f028b48ff08382b45635d937066
omar jakmira <poweromar19942@gmail.com>
2019-12-08 10:30:09 +0200

    custom types

--- basics/src/index.js
+++ basics/src/index.js
@@ -3,16 +3,43 @@ import { GraphQLServer } from "graphql-yoga"
 //type || application schema
 const typeDefs = `
     type Query {
-        hello:String!
+        me: User!
+        post:Post!
+    }
+
+    type User {
+        id:ID!
+        name:String!
+        email:String!
+        age:Int
+    }
+
+    type Post {
+        id:ID!
+        title:String!
+        body:String!
     }
 `
     //resolvers
 
 const resolvers = {
     Query: {
-        hello: () => {
-            return "Hello omar"
+        me() {
+            return {
+                id: 1,
+                name: "omar",
+                email: "o@o.com",
+                age: 20
+            }
+        },
+        post() {
+            return {
+                id: 1,
+                title: "post",
+                body: "some text"
+            }
         }
+
     }
 }
 
