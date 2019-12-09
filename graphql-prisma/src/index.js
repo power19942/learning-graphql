4ebbc5ab5365a84921a01351559a13bd553849bf
omar jakmira <poweromar19942@gmail.com>
2019-12-09 22:59:55 +0200

    connect prisma to node

---
+++ graphql-prisma/src/index.js
@@ -0,0 +1,31 @@
+import { GraphQLServer, PubSub } from 'graphql-yoga'
+import db from './db'
+import Query from './resolvers/Query'
+import Mutation from './resolvers/Mutations'
+import Post from './resolvers/Post'
+import Comment from './resolvers/Comment'
+import User from './resolvers/User'
+import Subscription from './resolvers/Subscription'
+
+const pubsub = new PubSub()
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers: {
+        Query,
+        Mutation,
+        Subscription,
+        Post,
+        Comment,
+        User
+    },
+    context: {
+        //pass db for each resolver in context argument
+        db,
+        pubsub
+    }
+})
+
+server.start(() => {
+    console.log('The server is up!')
+})
