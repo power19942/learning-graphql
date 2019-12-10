6ddfd84ffab8f2ea0c08fff39812503e6c6c38a6
omar jakmira <poweromar19942@gmail.com>
2019-12-10 21:53:41 +0200

    custom function for create and read

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -32,26 +32,81 @@ const prisma = new Prisma({
 //     )
 //     .then(data => console.log(JSON.stringify(data, undefined, 2)))
 
-prisma.mutation.createPost({
-    data: {
-        title: "another prisma post",
-        body: "Empty",
-        published: false,
-        author: {
-            connect: {
-                id: "ck3yvj8cc00200761mktr8z6z"
+// prisma.mutation.createPost({
+//     data: {
+//         title: "another prisma post",
+//         body: "Empty",
+//         published: false,
+//         author: {
+//             connect: {
+//                 id: "ck3yvj8cc00200761mktr8z6z"
+//             }
+//         }
+//     }
+// }, `
+// {
+//     id
+//     title
+//     body
+//     published
+//     author {
+//       id
+//       name
+//     }
+//   }
+// `).then(data => console.log(JSON.stringify(data, undefined, 2)))
+prisma.exists.Comment({
+    id: 'ck407dis6000m0761xw0lra42',
+    author: {
+        id: 'ck3yvj8cc00200761mktr8z6z'
+    }
+}).then(res => console.log(res))
+
+const createpostForUser = async(authorId, data) => {
+    const userExists = await prisma.exists.User({
+        id: 'ck3yvj8cc00200761mktr8z6z'
+    })
+
+    if (!userExists) {
+        throw new Error("user not found")
+    }
+    const post = await prisma.mutation.createPost({
+        data: {
+            ...data,
+            author: {
+                connect: {
+                    id: authorId
+                }
             }
         }
-    }
-}, `
-{
-    id
-    title
-    body
-    published
-    author {
-      id
-      name
-    }
-  }
-`).then(data => console.log(JSON.stringify(data, undefined, 2)))
+    }, `
+    {
+        id
+        title
+    }`)
+
+    const user = await prisma.query.user({
+        where: {
+            id: authorId
+        }
+    }, `
+        {
+            name
+            email
+            posts{
+                id
+                title
+                published
+            }
+        }
+    `)
+
+    return user
+}
+
+/*createpostForUser('ck3yvj8cc00200761mktr8z6z', {
+        title: 'greate book',
+        body: 'should read this book',
+        published: true
+    }).then(user => console.log(JSON.stringify(user, undefined, 2)))
+    .catch(err => console.log('error', err))*/
