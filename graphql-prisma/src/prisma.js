85d2b843ecf3fa4ba759a6207b7d7c88fe7b4929
omar jakmira <poweromar19942@gmail.com>
2019-12-10 20:55:21 +0200

    node create post by prisma

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,14 +5,53 @@ const prisma = new Prisma({
     endpoint: 'http://192.168.99.100:4466'
 })
 
-prisma.query.users(null, '{id name email posts{ id title}}')
-    .then((data) => {
-        console.log(JSON.stringify(data, undefined, 2))
-    })
+// prisma.query.users(null, '{id name email posts{ id title}}')
+//     .then((data) => {
+//         console.log(JSON.stringify(data, undefined, 2))
+//     })
 
-prisma.query.comments(null, `
+// prisma.query.comments(null, `
+// {
+//     id
+//     text
+// }
+// `).then(data => console.log(JSON.stringify(data, undefined, 2)))
+
+// prisma.mutation.createUser(
+
+//         {
+//             data: {
+//                 name: "omar2",
+//                 email: "o@o2.com"
+//             }
+//         },
+//         `{
+//             name
+//             email
+//         }`
+//     )
+//     .then(data => console.log(JSON.stringify(data, undefined, 2)))
+
+prisma.mutation.createPost({
+    data: {
+        title: "another prisma post",
+        body: "Empty",
+        published: false,
+        author: {
+            connect: {
+                id: "ck3yvj8cc00200761mktr8z6z"
+            }
+        }
+    }
+}, `
 {
     id
-    text
-}
+    title
+    body
+    published
+    author {
+      id
+      name
+    }
+  }
 `).then(data => console.log(JSON.stringify(data, undefined, 2)))
